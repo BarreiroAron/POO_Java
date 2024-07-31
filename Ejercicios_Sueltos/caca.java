776b9a1d4da4ffaa0330759490ea8d057bcd4185
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-07-31 12:07:28 -0300

    Create caca.java
    xd

---
+++ Ejercicios_Sueltos/caca.java
@@ -0,0 +1,10 @@
+public class caca {
+	public static void main(String[] args) {
+	int y = 45 + (25 * 10);
+	System.out.println(" " + y);
+	float z = (1 / 3);
+	System.out.println(" " + z);
+	float a = 1 / (3 + a);
+	System.out.println(" " + a);
+	}
+}
