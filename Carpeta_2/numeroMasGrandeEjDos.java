2bf382d68ba25612338756536554c4b198e4a929
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-04 12:16:59 -0300

    Create numeroMasGrandeEjDos.java
    hacemos el ejercicio cdos chavales x

---
+++ Carpeta_2/numeroMasGrandeEjDos.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class numeroMasGrandeEjDos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese dos numeros por faovr: ");
+        
+        int numeroUno = scanner.nextInt();
+        int numeroDos = scanner.nextInt();
+        if(numeroUno > numeroDos) {
+            System.out.println("EL PRIMER NUMERO ES MAYOR CHAVALES");
+        } else if (numeroUno == numeroDos) {
+            System.out.println("SON LOS DOS IGUALES CHAVALES");
+        } else {
+            System.out.println("EL DOS NUMERO ES MAS GRANDE CHAVALON");
+        }
+    }
+}
