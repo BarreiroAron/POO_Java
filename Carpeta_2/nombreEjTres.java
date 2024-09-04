489ee4adcaf5589cd183c5826297cfedb3c5900a
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-04 12:20:04 -0300

    Create nombreEjTres.java
    nobres

---
+++ Carpeta_2/nombreEjTres.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class nombreEjTres {
+    public static void main(String[] args) {
+        String nombre = "Caca";
+        System.out.println("Bienvenido " + nombre);
+    }
+}
