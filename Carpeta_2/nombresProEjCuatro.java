f0d7a19023a90febac151831d18243da9e8fdc20
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-04 12:30:39 -0300

    Create nombresProEjCuatro.java
    hacemos el anterior ejercicio pero con un agregado de que nosotros podemos introducir el nombre que queramos

---
+++ Carpeta_2/nombresProEjCuatro.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class nombresProEjCuatro {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Te pedimos el nombre porfa :v ");
+        String hola = scanner.nextLine();
+        System.out.println("Bienvenido " + hola + " ! !");
+    }
+}
