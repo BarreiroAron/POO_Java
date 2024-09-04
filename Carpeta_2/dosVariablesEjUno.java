32ad71f8d8aea847b640753bd55101f72f07a9de
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-04 11:50:30 -0300

    Update dosVariablesEjUno.java
    xd

--- Carpeta_2/dosVariablesEjUno.java
+++ Carpeta_2/dosVariablesEjUno.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class hola {
+public class dosVariablesEjUno {
     public static void main(String[] args) {
         System.out.println("   ---------------");
         System.out.println("  | 1     2     3 |");
