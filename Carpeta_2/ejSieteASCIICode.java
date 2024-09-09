e981526d97300a980781297c0f3568320de35a04
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-09 17:29:08 -0300

    Create ejSieteASCIICode.java
    NO FUNCIONA EN ONLINE JAVAAAAA :ccCCCCC

---
+++ Carpeta_2/ejSieteASCIICode.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JOptionPane;
+
+public class ejSieteASCIICode {
+    public static void main(String[] args) {
+        String texto = JOptionPane.showInputDialog("Introduce un código de la tabla ASCII");
+        //Este showInputDialog es como un alert de un html. 
+        //No funciona en online java :C   
+        int codigo = 0;
+        for (int i = 0; i < texto.length(); i++) { //tampooc funciona en online.java
+            char c = texto.charAt(i); //tampoco funciona en online.java
+            int digito = c - '0';
+            codigo = codigo * 10 + digito;
+        }
+        char caracter = (char) codigo;
+        System.out.println(caracter);
+    }
+}
