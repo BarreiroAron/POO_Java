9324e9564e7d70f221131c0dfc8a9ca2395edcbc
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-09 17:43:06 -0300

    Create pruebaLenght.java
    estoy probando lenght para entenderlo mejor

---
+++ Ejercicios_Sueltos/pruebaLenght.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+String texto = "papa";
+int longitud = texto.length();
+System.out.println("La longitud de la cadena es: " + longitud);
+}}
