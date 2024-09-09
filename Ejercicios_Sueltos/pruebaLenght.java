94772f99a76b850a7c17fd5c13413ae503f8a484
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-09 17:44:46 -0300

    Update pruebaLenght.java
    agtrengando comentarios

--- Ejercicios_Sueltos/pruebaLenght.java
+++ Ejercicios_Sueltos/pruebaLenght.java
@@ -4,3 +4,7 @@ String texto = "papa";
 int longitud = texto.length();
 System.out.println("La longitud de la cadena es: " + longitud);
 }}
+//Agarra textos y lo traduce a numeros, segun cuantas letras tenga en la palabra...
+//Si la palabra es "Hola", va a tomar 4 letras y va a imprimir 4
+//En caso de ser algo como: Hola como esta", entonces va a contar los espacios y todas las palabras
+//Hola: 4, espacio: 1, como: 4, espacio: 1, esta: 4 = 14 letras.
