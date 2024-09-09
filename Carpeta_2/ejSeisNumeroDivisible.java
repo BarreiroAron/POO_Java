9b4a081528dbd4d939fc48ef77fc3e75af7c423c
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-09 17:02:39 -0300

    Create ejSeisNumeroDivisible.java
    agregamos otro ejercicio mas a la lista

---
+++ Carpeta_2/ejSeisNumeroDivisible.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ejSeisNumeroDivisible {
+    public static void main(String[] args) {
+	Scanner numero = new Scanner(System.in);
+	
+	System.out.println("Elija un numero");
+	int numeroIngresar = numero.nextInt();
+	System.out.println("El numero ingresado es " + numeroIngresar);
+	int divisible = numeroIngresar % 2;
+	if(divisible == 0) {
+		System.out.println("Es par");
+	} else {
+		System.out.println("Es impar xd");
+	}
+    }
+}
