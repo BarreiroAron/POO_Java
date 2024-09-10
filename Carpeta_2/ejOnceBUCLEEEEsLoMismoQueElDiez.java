f586d4c385d8dbfb2008b90181623f0b08fb46b5
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-10 14:17:02 -0300

    Create ejOnceBUCLEEEEsLoMismoQueElDiez.java
    xzd

---
+++ Carpeta_2/ejOnceBUCLEEEEsLoMismoQueElDiez.java
@@ -0,0 +1,22 @@
+package elvideodiseotracosa;
+
+import java.util.Scanner;
+
+public class ejOnceBUCLEEEEsLoMismoQueElDiez {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 100; i++) {
+			System.out.println(i);
+		}
+		System.out.println("Quiere hacerlo pero con while? Xd. Si es asi, ingrese 1 por favor");
+		int decidir;
+		Scanner xd = new Scanner(System.in);
+		decidir = xd.nextInt();
+		if(decidir == 1) {
+			int j = 1;
+			while(j <= 100) {
+				System.out.println(j);
+				j++;
+			}
+		}
+	}
+}
