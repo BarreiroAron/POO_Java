84702a1e09b081d3e729da07cebafa15034cb31c
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-10 14:47:03 -0300

    Create ejTreceComprarCostias.java
    xd

---
+++ Carpeta_2/ejTreceComprarCostias.java
@@ -0,0 +1,18 @@
+package elvideodiseotracosa;
+
+import java.util.Scanner;
+
+public class ejTreceComprarCostias {
+	public static void main(String[] args) {
+		Scanner r = new Scanner(System.in);
+		System.out.println("Introduzca cuantas cozas compro :v");
+		int precio = 0, precioTotal = 0;
+		int cositas = r.nextInt();
+		for(int i = 0; i < cositas; i++) {
+			precio = r.nextInt();
+			precioTotal += precio;
+			precio = 0;
+		}
+		System.out.println("Mira todo lo que gastaste man: " + precioTotal + " Hyper banca rota en plan brooo");
+	   }
+	}
