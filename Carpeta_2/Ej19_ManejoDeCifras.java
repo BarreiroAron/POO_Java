5f6ce5e14afab3cb3bfb40808cefdea10f18cced
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-19 08:12:57 -0300

    Create Ej19_ManejoDeCifras.java
    zd

---
+++ Carpeta_2/Ej19_ManejoDeCifras.java
@@ -0,0 +1,13 @@
+package holaComoEstas;
+
+import java.util.Scanner;
+
+public class Ej19_ManejoDeCifras {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un numero o palabra: ");
+        String numero = scanner.nextLine();
+        int numeroCifras = numero.length();
+        System.out.println("El numero o palabra tiene " + numeroCifras + " cifras.");
+    }
+}
