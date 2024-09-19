cee0769b367d2d1d1b861dbaf955ce8b04967df2
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-19 08:48:09 -0300

    Update ej4_NombresPro.java
    xd

--- Carpeta_2/ej4_NombresPro.java
+++ Carpeta_2/ej4_NombresPro.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ejCuatroNombresPro {
+public class ej4_NombresPro {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Te pedimos el nombre porfa :v ");
