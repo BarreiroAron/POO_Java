fe38189688326958f4394ceafd4f11dc5a40fd36
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-19 08:53:52 -0300

    Update and rename ejUnoDosVariables.java to ej1_DosVariables.java
    xd

--- Carpeta_2/ej1_DosVariables.java
+++ Carpeta_2/ej1_DosVariables.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ejUnoDosVariables {
+public class ej1_DosVariables {
     public static void main(String[] args) {
         System.out.println("   ---------------");
         System.out.println("  | 1     2     3 |");
