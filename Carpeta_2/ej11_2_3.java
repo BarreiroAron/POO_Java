95a57f41b0f2cf2e6bebcaf29390d6c459af6ce0
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-19 08:52:00 -0300

    Update and rename ejOnceDosYTres.java to ej11_2_3.java
    xd

--- Carpeta_2/ej11_2_3.java
+++ Carpeta_2/ej11_2_3.java
@@ -1,6 +1,6 @@
 package elvideodiseotracosa;
 
-public class ejOnceDosYTres {
+public class ej11_2_3 {
 	public static void main(String[] args) {
 		for(int i = 1; i <= 100; i++) {
 			if(i % 2 == 0) {
