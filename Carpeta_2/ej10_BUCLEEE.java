04aeace66c2cde2d9c755682c9c4b44a15a1c7dd
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-19 08:50:24 -0300

    Update and rename ejDiezBUCLEEE.java to ej10_BUCLEEE.java
    xd

--- Carpeta_2/ej10_BUCLEEE.java
+++ Carpeta_2/ej10_BUCLEEE.java
@@ -2,7 +2,7 @@ package elvideodiseotracosa;
 
 import java.util.Scanner;
 
-public class ejDiezBUCLEEE {
+public class ej10_BUCLEEE {
 	public static void main(String[] args) {
 		for(int i = 1; i <= 100; i++) {
 			System.out.println(i);
