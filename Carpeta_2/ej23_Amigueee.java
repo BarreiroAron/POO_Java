e3852d0ee4e6f261ade784da97c732d6bd093686
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-22 19:43:24 -0300

    Create ej23_Amigueee.java
    xd

---
+++ Carpeta_2/ej23_Amigueee.java
@@ -0,0 +1,8 @@
+public class ej23_Amigueee {
+    public static void main(String[] args) {
+        String[] array = new String[1];
+        array[0] = "La lluvia en Sevilla es una maravilla";
+        array[0] = array[0].replace('a', 'e');
+        System.out.println(array[0]);
+    }
+}
