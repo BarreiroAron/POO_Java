7ca471f3e00fb367d35ce9e1922666bbf8eec669
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-22 21:56:27 -0300

    Create ej30_ConvertirEnMayusculas.java
    xd

---
+++ Carpeta_2/ej30_ConvertirEnMayusculas.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ej30_ConvertirEnMayusculas {
+    public static void main(String[] args) {
+    	Scanner scanner = new Scanner(System.in);
+    	String hola = scanner.nextLine();
+    	System.out.println(hola.toUpperCase());
+    }
+}
