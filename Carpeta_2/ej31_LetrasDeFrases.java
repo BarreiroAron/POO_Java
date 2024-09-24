087ce82888ea706f27b0cca920e4d60f62a1a7c2
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-09-24 14:16:09 -0300

    Create ej31_LetrasDeFrases.java
    xdf

---
+++ Carpeta_2/ej31_LetrasDeFrases.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ej31 {
+	public static void main(String[] args) {
+	System.out.println("Introduzca una frase");
+	Scanner scanner = new Scanner(System.in);
+	String palabra = scanner.nextLine();
+	System.out.println("La palabra tiene (Incluyendo espacios) " + palabra.length() + " letras.");
+	}
+}
